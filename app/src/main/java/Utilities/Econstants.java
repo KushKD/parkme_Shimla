9bb9f5ad0385bc4272a3111c83545569cb2f6994
kuush <kushkumardhawan@gmail.com>
2016-06-22 15:10:04 +0530

    -- Updated App

--- app/src/main/java/Utilities/Econstants.java
+++ app/src/main/java/Utilities/Econstants.java
@@ -6,11 +6,10 @@ package Utilities;
 public class Econstants {
 
     //Production
-   /* public static final String URL_MAIN = "http://www.hpparking.hp.gov.in/HPParking.svc";
+    public static final String URL_MAIN = "http://www.hpparking.hp.gov.in/HPParking.svc";
     public static final String URL_GENERIC = "http://hpparking.hp.gov.in/HPParking.svc/getparkingLocation_JSON/10297";
     public static final String URL_Rates_Small = "http://hpparking.hp.gov.in/HPParking.svc/getParkingFeeParkingId_JSON/";
     public static final String URL_Rates_Big = "http://hpparking.hp.gov.in/HPParking.svc/getParkingFeeParkingId_JSON/";
-*/
 
     public static final String PREFRANCE_NAME = "Maps_Registration";
 
@@ -21,11 +20,11 @@ public class Econstants {
     public static final String URL_Rates_Big = "http://192.168.0.171/HPParking/HPParking.svc/getParkingFeeParkingId_JSON/";*/
 
     //Kush PC
-    public static final String URL_GENERIC = "http://192.168.0.197:7002/HPParking.svc/getparkingLocation_JSON/10297";
+  /*  public static final String URL_GENERIC = "http://192.168.0.197:7002/HPParking.svc/getparkingLocation_JSON/10297";
     public static final String  URL_MAIN = "http://192.168.0.197:7002/HPParking.svc";
     public static final String URL_Rates_Small = "http://192.168.0.197:7002/HPParking.svc/getParkingFeeParkingId_JSON/";
     public static final String URL_Rates_Big = "http://192.168.0.197:7002/HPParking.svc/getParkingFeeParkingId_JSON/";
-
+*/
 }
 
 
