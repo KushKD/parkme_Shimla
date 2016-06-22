602eb8bc6a59172b66bac67497d657ab77d805ae
kuush <kushkumardhawan@gmail.com>
2016-06-22 20:29:33 +0530

    -- Firebase Integrated

---
+++ app/src/main/java/findparking/hp/dit/himachal/com/shimlaparking/MyFirebaseInstanceIDService.java
@@ -0,0 +1,32 @@
+package findparking.hp.dit.himachal.com.shimlaparking;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by kuush on 6/22/2016.
+ */
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+
+    private static final String TAG = "MyFirebaseIIDService";
+
+    @Override
+    public void onTokenRefresh() {
+
+        //Getting registration token
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+        //Displaying token on logcat
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+    }
+
+    private void sendRegistrationToServer(String token) {
+        //You can implement this method to store the token on your server
+        //Not required for current project
+    }
+
+}
