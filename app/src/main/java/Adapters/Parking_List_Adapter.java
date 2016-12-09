9571d9e1ca4cf9faa154fcab06b5407980a1ec12
kushkd <kushkumardhawan@gmail.com>
2016-12-09 11:52:17 +0530

    -- All Tasks Completed

--- app/src/main/java/Adapters/Parking_List_Adapter.java
+++ app/src/main/java/Adapters/Parking_List_Adapter.java
@@ -2,11 +2,15 @@ package Adapters;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 
 import java.util.List;
 
@@ -35,13 +39,32 @@ public class Parking_List_Adapter extends ArrayAdapter<Parking_List_Pojo> {
         Parking_List_Pojo RO = userlist.get(position);
         TextView tv1 = (TextView) view.findViewById(R.id.itemone);
         TextView tv2 = (TextView) view.findViewById(R.id.itemtwo);
+        ImageView imageView1 = (ImageView) view.findViewById(R.id.imageView1);
+
+        if (RO.getAvailability().equalsIgnoreCase("Not Known")) {
+           // tv1.setTextColor(Color.YELLOW);
+           // tv2.setTextColor(Color.YELLOW);
+            imageView1.setImageResource(R.mipmap.yellow);
+        } else {
+            //Toast.makeText(getApplicationContext(),myMarker.getParkingFullTag().toString(),Toast.LENGTH_LONG).show();
+            // available.setText(myMarker.getAvailability()+"("+myMarker.getPercentage()+"%)");  //
+            // parking_availability.setTextColor(Color.parseColor("#ffa500")); //orange
+            if (RO.getPercentage().equalsIgnoreCase("0")) {
+                //tv1.setTextColor(Color.RED);
+               // tv2.setTextColor(Color.RED);
+                imageView1.setImageResource(R.mipmap.red);
+            } else {
+               // tv1.setTextColor(Color.GREEN);
+               // tv2.setTextColor(Color.GREEN);
+                imageView1.setImageResource(R.mipmap.green);
+            }
+        }
+            tv1.setText(RO.getParkingPlace());
+            tv2.setText(RO.getIdentifier());
 
 
-        tv1.setText(RO.getParkingPlace());
-        tv2.setText(RO.getIdentifier());
+            return view;
+        }
 
-
-        return view;
-    }
 }
 
